b1347030da2e7ccf5f627afde2ff19ba820b529c
Pratik Narode <pratik.narode@macrometa.co>
2019-01-24 16:50:03 +0530

    Bump pyC8 driver version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./README.md') as fp:
 
 setup(
     name='pyC8',
-    version='0.10.2',
+    version='0.10.3',
     description='Python Driver for Macrometa Digital Edge Fabric',
     long_description=description,
     author='Macrometa',
