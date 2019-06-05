dc8d91f3fe8316911c6aa957f3db5d2c22405ee6
Pratik Narode <pratik.narode@macrometa.co>
2019-06-05 12:21:49 +0530

    saved queries: add validation for white spaces

--- c8/exceptions.py
+++ c8/exceptions.py
@@ -574,6 +574,9 @@ class PermissionResetError(C8ServerError):
 #####################
 
 
+class RestqlValidationError(C8ClientError):
+    """Failed to validate restql data."""
+
 class RestqlCreateError(C8ServerError):
     """Failed to create restql."""
 
