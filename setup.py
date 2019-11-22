0727fc3fabe923e19944959a815a897c11111641
Sulom <49052810+sulomMacrometa@users.noreply.github.com>
2019-11-22 10:52:03 +0530

    Bumping version to 0.14.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./README.md') as fp:
 
 setup(
     name='pyC8',
-    version='0.14.3',
+    version='0.14.4',
     description='Python Driver for Macrometa Global Edge Fabric',
     long_description=description,
     long_description_content_type="text/markdown",
