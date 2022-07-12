a64d82cd43911b849d419bab209ee8a637d62f1b
GitHub Action <github-actions@github.com>
2022-07-12 18:28:33 +0000

    0.17.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./README.md') as fp:
 
 setup(
     name='pyC8',
-    version='0.16.9',
+    version='0.17.0',
     description='Python Driver for Macrometa Global Edge Fabric',
     long_description=description,
     long_description_content_type="text/markdown",
