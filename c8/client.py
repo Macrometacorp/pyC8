b8a066008824d5b675e63c818c097922273c3b81
koshy-thomas <107413064+koshy-thomas@users.noreply.github.com>
2022-10-07 01:49:50 +0530

    Adding test_apikeys.py and updating test_permission.py (#99)
    * Update test_permission.py
    
    * Update apikeys.py
    
    * Update client.py
    
    * Update exceptions.py
    
    * Update fabric.py
    
    * Update tenant.py
    
    * Update conftest.py
    
    * Update helpers.py
    
    * Update helpers.py
    
    * Update tenant.py
    
    * Update client.py
    
    * Update apikeys.py
    
    * Update fabric.py
    
    * Update conftest.py
    
    * Create test_apikeys.py
    
    * Update apikeys.py

--- c8/client.py
+++ c8/client.py
@@ -2004,7 +2004,7 @@ class C8Client(object):
         :type databasename: str | unicode
         :returns: Access Details
         :rtype: string
-        :raise c8.exceptions.DataBaseError: If request fails.
+        :raise c8.exceptions.GetDataBaseAccessLevel: If request fails.
         """
         return self._tenant.get_database_access_level_user(username=username,
                                                            databasename=databasename)
@@ -2021,7 +2021,7 @@ class C8Client(object):
         :type databasename: str | unicode
         :returns: Object containing database details
         :rtype: object
-        :raise c8.exceptions.DataBaseError: If request fails.
+        :raise c8.exceptions.ClearDataBaseAccessLevel: If request fails.
         """
         return self._tenant.remove_database_access_level_user(username=username,
                                                               databasename=databasename)
@@ -2042,11 +2042,30 @@ class C8Client(object):
         :type grant: string
         :returns: Object containing database details
         :rtype: object
-        :raise c8.exceptions.DataBaseError: If request fails.
+        :raise c8.exceptions.SetDataBaseAccessLevel: If request fails.
         """
         return self._tenant.set_database_access_level_user(username=username,
                                                            databasename=databasename,
                                                            grant=grant)
+
+    # client.list_accessible_collections_user
+
+    def list_accessible_collections_user(self, username, databasename='_system', full=False):
+        """Fetch the collection access level for a specific collection in a database.
+
+        :param username: Name of the user
+        :type username: string
+        :param databasename: Name of the database
+        :type databasename: string
+        :param full: Return the full set of access levels for all collections.
+        :type full: boolean
+        :returns: Fetch the list of collections access level for a specific user.
+        :rtype: string
+        :raise c8.exceptions.CollectionAccessLevel: If request fails.
+        """
+        return self._tenant.list_accessible_collections_user(username=username,
+                                                             databasename=databasename,
+                                                             full=full)
 
     # client.get_collection_access_level_user
 
@@ -2223,7 +2242,7 @@ class C8Client(object):
         :rtype: Object
         :raise c8.exceptions.SetBillingAccessLevel: If request fails.
         """
-        return self._tenant.set_billing_access_level(username=username, grant=grant)
+        return self._tenant.set_billing_access_level_user(username=username, grant=grant)
 
     # client.clear_billing_access_level
 
@@ -2235,7 +2254,7 @@ class C8Client(object):
         :rtype: booleaan
         :raise c8.exceptions.ClearBillingAccessLevel: If request fails.
         """
-        return self._tenant.clear_billing_access_level(username=username)
+        return self._tenant.clear_billing_access_level_user(username=username)
 
     # client.get_attributes_user
 
@@ -2259,7 +2278,7 @@ class C8Client(object):
         :type attributes: dict
         :returns: The updated attributes.
         :rtype: Object
-        :raise c8.exceptions.GetAUpdateAttributesttributes: If request fails.
+        :raise c8.exceptions.UpdateAttributes: If request fails.
         """
         return self._tenant.update_attributes_user(username=username,
                                                    attributes=attributes)
@@ -2460,6 +2479,12 @@ class C8Client(object):
     def list_all_api_keys(self):
         return self._fabric.list_all_api_keys()
 
+    # client.get_api_key
+
+    def get_api_key(self, keyid):
+        _apiKeys = self._fabric.api_keys(keyid)
+        return _apiKeys.get_api_key()
+
     # client.remove_api_key
 
     def remove_api_key(self, keyid):
@@ -2477,7 +2502,6 @@ class C8Client(object):
     def get_database_access_level(self, keyid, databasename):
         """Fetch the database access level for a specific database.
 
-
         :param databasename: Name of the database
         :type databasename: string
         :returns: AccessLevel of a db.
@@ -2494,6 +2518,10 @@ class C8Client(object):
     def clear_database_access_level(self, keyid, databasename):
         _apiKeys = self._fabric.api_keys(keyid)
         return _apiKeys.clear_database_access_level(databasename)
+
+    def list_accessible_collections(self, keyid, databasename='_system', full=False):
+        _apiKeys = self._fabric.api_keys(keyid)
+        return _apiKeys.list_accessible_collections(databasename, full)
 
     def get_collection_access_level(self, keyid, collection_name,
                                     databasename='_system'):
@@ -2543,6 +2571,22 @@ class C8Client(object):
     def clear_billing_access_level(self, keyid):
         _apiKeys = self._fabric.api_keys(keyid)
         return _apiKeys.clear_billing_access_level()
+
+    def get_attributes(self, keyid):
+        _apiKeys = self._fabric.api_keys(keyid)
+        return _apiKeys.get_attributes()
+
+    def update_attributes(self, keyid, attributes):
+        _apiKeys = self._fabric.api_keys(keyid)
+        return _apiKeys.update_attributes(attributes)
+
+    def remove_all_attributes(self, keyid):
+        _apiKeys = self._fabric.api_keys(keyid)
+        return _apiKeys.remove_all_attributes()
+
+    def remove_attribute(self, keyid, attributeid):
+        _apiKeys = self._fabric.api_keys(keyid)
+        return _apiKeys.remove_attribute(attributeid)
 
     def set_search(self, collection, enable, field):
         """Set search capability of a collection (enabling or disabling it). 
