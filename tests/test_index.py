864b8be183ff188d85b250e5cc83aced9fa24b0c
edgargarciamacrometa <107628437+edgargarciamacrometa@users.noreply.github.com>
2022-10-12 17:34:07 -0400

    Update test_index.py
    added test case when the collection is global and set unique to true and unique to false. Modified the parameter index_id by index_name in the function delete_index(), the last one is what the server is waiting to delete the index

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,47 +1,44 @@
 from __future__ import absolute_import, unicode_literals
-
 from c8.exceptions import (
-    IndexListError,
     IndexCreateError,
     IndexDeleteError,
 )
 from tests.helpers import assert_raises, extract
 
 
-def test_list_indexes(col, bad_col):
+def test_list_indexes(col):
+    # Test default primary index
     expected_index = {
         'id': '0',
         'selectivity': 1,
         'sparse': False,
         'type': 'primary',
         'fields': ['_key'],
-        'unique': True
+        'unique': True,
+        'name': 'primary'
     }
     indexes = col.indexes()
     assert isinstance(indexes, list)
     assert expected_index in indexes
 
-    with assert_raises(IndexListError) as err:
-        bad_col.indexes()
-    assert err.value.error_code == 1228
-
 
 def test_add_hash_index(col):
     fields = ['attr1', 'attr2']
-    result = col.add_hash_index(
-        fields=fields,
-        unique=True,
-        sparse=True,
-        deduplicate=True
-    )
+    # Test if unique set to true in global collection
+    with assert_raises(IndexCreateError) as err:
+        col.add_hash_index(fields=fields, unique=True, sparse=True, deduplicate=True)
+    assert err.value.error_code == 400
 
+    # Test if unique set to false in global collection
+    result = col.add_hash_index(fields=fields, unique=False, sparse=True, deduplicate=True)
     expected_index = {
         'selectivity': 1,
         'sparse': True,
         'type': 'hash',
         'fields': ['attr1', 'attr2'],
-        'unique': True,
-        'deduplicate': True
+        'unique': False,
+        'deduplicate': True,
+        'new': True,
     }
     for key, value in expected_index.items():
         assert result[key] == value
@@ -52,19 +49,25 @@ def test_add_hash_index(col):
 
 def test_add_skiplist_index(col):
     fields = ['attr1', 'attr2']
+    # Test if unique set to true in global collection
+    with assert_raises(IndexCreateError) as err:
+        col.add_skiplist_index(fields=fields, unique=True, sparse=True, deduplicate=True)
+    assert err.value.error_code == 400
+
+    # Test if unique set to false in global collection
     result = col.add_skiplist_index(
         fields=fields,
-        unique=True,
+        unique=False,
         sparse=True,
         deduplicate=True
     )
-
     expected_index = {
         'sparse': True,
         'type': 'skiplist',
         'fields': ['attr1', 'attr2'],
-        'unique': True,
-        'deduplicate': True
+        'unique': False,
+        'deduplicate': True,
+        'new': True
     }
     for key, value in expected_index.items():
         assert result[key] == value
@@ -82,12 +85,11 @@ def test_add_geo_index(col):
 
     expected_index = {
         'sparse': True,
-        'type': 'geo1',
+        'type': 'geo',
         'fields': ['attr1'],
         'unique': False,
         'geo_json': False,
-        'ignore_none': True,
-        'constraint': False
+        'new': True,
     }
     for key, value in expected_index.items():
         assert result[key] == value
@@ -102,11 +104,9 @@ def test_add_geo_index(col):
     )
     expected_index = {
         'sparse': True,
-        'type': 'geo2',
+        'type': 'geo',
         'fields': ['attr1', 'attr2'],
         'unique': False,
-        'ignore_none': True,
-        'constraint': False
     }
     for key, value in expected_index.items():
         assert result[key] == value
@@ -146,17 +146,23 @@ def test_add_fulltext_index(col):
 
 
 def test_add_persistent_index(col):
+    fields = ['attr1', 'attr2']
+    # Test if unique set to true in global collection
+    with assert_raises(IndexCreateError) as err:
+        col.add_persistent_index(fields=fields, unique=True, sparse=True)
+    assert err.value.error_code == 400
+
     # Test add persistent index with two attributes
     result = col.add_persistent_index(
-        fields=['attr1', 'attr2'],
-        unique=True,
+        fields=fields,
+        unique=False,
         sparse=True,
     )
     expected_index = {
         'sparse': True,
         'type': 'persistent',
         'fields': ['attr1', 'attr2'],
-        'unique': True,
+        'unique': False,
     }
     for key, value in expected_index.items():
         assert result[key] == value
@@ -166,19 +172,19 @@ def test_add_persistent_index(col):
 
 
 def test_delete_index(col, bad_col):
-    old_indexes = set(extract('id', col.indexes()))
-    col.add_hash_index(['attr3', 'attr4'], unique=True)
-    col.add_skiplist_index(['attr3', 'attr4'], unique=True)
+    old_indexes = set(extract('name', col.indexes()))
+    col.add_hash_index(['attr3', 'attr4'], unique=False)
+    col.add_skiplist_index(['attr3', 'attr4'], unique=False)
     col.add_fulltext_index(fields=['attr3'], min_length=10)
 
-    new_indexes = set(extract('id', col.indexes()))
+    indexes = col.indexes()
+    new_indexes = set(extract('name', indexes))
     assert new_indexes.issuperset(old_indexes)
-
     indexes_to_delete = new_indexes - old_indexes
-    for index_id in indexes_to_delete:
-        assert col.delete_index(index_id) is True
+    for index_name in indexes_to_delete:
+        assert col.delete_index(index_name) is True
 
-    new_indexes = set(extract('id', col.indexes()))
+    new_indexes = set(extract('name', col.indexes()))
     assert new_indexes == old_indexes
 
     # Test delete missing indexes
@@ -188,11 +194,3 @@ def test_delete_index(col, bad_col):
         with assert_raises(IndexDeleteError) as err:
             col.delete_index(index_id, ignore_missing=False)
         assert err.value.error_code == 1212
-
-    # Test delete indexes with bad collection
-    for index_id in indexes_to_delete:
-        with assert_raises(IndexDeleteError) as err:
-            bad_col.delete_index(index_id, ignore_missing=False)
-        assert err.value.error_code == 1228
-
-
