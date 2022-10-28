3602a6a490b0736f9bd9189aee41825d6dd7707c
koshy-thomas <107413064+koshy-thomas@users.noreply.github.com>
2022-10-28 18:32:58 +0530

    Update url_tester.py

--- tests/url_tester.py
+++ tests/url_tester.py
@@ -1,27 +1,29 @@
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError, URLError
 
-try:
-    req = Request(
-             url = "https://macrometa.com/docs/",
-             headers = {"User-Agent": "Mozilla/5.0"}
-          )
 
-    # Add your urls below
-    urls = [
-        "https://macrometa.com/docs/account-management/api-keys/",
-        "https://macrometa.com/docs/account-management/auth/user-auth"
-    ]
+req = Request(
+        url = "https://macrometa.com/docs/",
+        headers = {"User-Agent": "Mozilla/5.0"}
+      )
 
-    for url in urls:
-        req.full_url = url
-        urlopen(req)
+# Add your urls below
+urls = [
+    "https://macrometa.com/docs/account-management/api-keys/",
+    "https://macrometa.com/docs/account-management/auth/user-auth"
+]
 
-except HTTPError as e:
-    print("Page down: ", req.full_url, " ", e)
+is_broken = False
+for url in urls:
+    req.full_url = url
+    try:
+        resp = urlopen(req)
+    except HTTPError as e:
+        is_broken = True
+        print("Page down: ", req.full_url, " ", e)
+    except URLError as e:
+        is_broken = True
+        print("Page down: ", req.full_url, " ", e)
 
-except URLError as e:
-    print("Page down: ", req.full_url, " ", e)
-
-else:
+if(is_broken is False):
     print("All pages are up!")
